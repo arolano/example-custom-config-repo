b2b1c6f6a6089191c7c96bfc612925b7e86cad68
arolano <aaootteerr@mail.ru>
2021-12-21 12:44:37 +0300

    re-applying microg in 12th branch
    Re-added microg in 12th

--- hooks/aosp_build_pre.sh
+++ hooks/aosp_build_pre.sh
@@ -2,11 +2,12 @@
 
 cd "${AOSP_BUILD_DIR}"
 
-#echo "applying microg patches"
-#cd "${AOSP_BUILD_DIR}/packages/modules/Permission"
-#patch -p1 --no-backup-if-mismatch < "${AOSP_BUILD_DIR}/platform/prebuilts/microg/00001-fake-package-sig.patch"
-#cd "${AOSP_BUILD_DIR}/frameworks/base"
-#patch -p1 --no-backup-if-mismatch < "${AOSP_BUILD_DIR}/platform/prebuilts/microg/00002-microg-sigspoof.patch"
+#Re-adding microg in 12th branch too
+echo "applying microg patches"
+cd "${AOSP_BUILD_DIR}/packages/modules/Permission"
+patch -p1 --no-backup-if-mismatch < "${AOSP_BUILD_DIR}/platform/prebuilts/microg/00001-fake-package-sig.patch"
+cd "${AOSP_BUILD_DIR}/frameworks/base"
+patch -p1 --no-backup-if-mismatch < "${AOSP_BUILD_DIR}/platform/prebuilts/microg/00002-microg-sigspoof.patch"
 
 patch_mkbootfs(){
   cd "${AOSP_BUILD_DIR}/system/core"
